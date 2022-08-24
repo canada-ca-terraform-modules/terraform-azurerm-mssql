8bd3d0c3e6c5982acd0cab752e054119e3e6842f
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2022-08-24 15:46:38 +0000

    remove dbowners unused

--- main.tf
+++ main.tf
@@ -47,7 +47,6 @@ module "db" {
   server_id                              = module.sqlserver[0].id
   server_name                            = module.sqlserver[0].name
   sku_name                               = var.db_sku_names[count.index]
-  dbowners                                = var.dbowners
   kv_name                                = var.kv_name
   kv_rg                                  = var.kv_resource_group_name
   storageaccountinfo_resource_group_name = var.storageaccountinfo_resource_group_name
