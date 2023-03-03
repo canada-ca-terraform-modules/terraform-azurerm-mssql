fc727e69e8d192e47e29a54a8d986a33454289c3
sama.mahmoud <sama.mahmoud@cloud.statcan.ca>
2023-03-03 17:13:59 +0000

    remove tag

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "sqlserver" {
   active_directory_administrator_tenant_id      = var.active_directory_administrator_tenant_id
   emails                                        = var.emails
   private_endpoint_subnet                       = var.private_endpoint_subnet
-  tags                                          = var.tags
+  #tags                                          = var.tags
 }
 
 module "elasticpool" {
