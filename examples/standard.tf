38446ede44e9d87b15ed0886675b38f586c983c0
Sama Mahmoud <sama.mahmoud@cloud.statcan.ca>
2023-03-20 19:25:42 +0000

    update version in example

--- examples/standard.tf
+++ examples/standard.tf
@@ -1,5 +1,5 @@
 module "mssql" {
-  source = "git::https://gitlab.k8s.cloud.statcan.ca/managed-databases/terraform-azurerm-mssql?ref=test_sama"
+  source = "git::https://gitlab.k8s.cloud.statcan.ca/managed-databases/single-server/terraform-azurerm-mssql?ref=v2.0.3"
 
   mssql_name          = "sqlservername001"
   location            = "canadacentral"
